9f15cdf92166a12e3b6ed62cf5cea4810d383c21
Danny <ddeneau@terpmail.umd.edu>
2023-03-31 02:45:27 -0400

    Changed to a Flask app for Heroku deployment
    Restructured code so that it works as Flask app.

--- reviewcounter.py
+++ reviewcounter.py
@@ -1,25 +1,34 @@
 import googlemaps
-import json
+from flask import Flask, jsonify
 import requests
 from datetime import datetime
 
-counter = 0
-# Replace key='[insert your key without brackets]'
-gmaps = googlemaps.Client(key='AIzaSyDDKS_pV3ihaftl-2JRVEpqMlOZe8I0yp4')
-# Specify the place ID of the business
-place_id = 'ChIJ7fLdskyTToYR81Dj4KgYsds'
-# Get the place details, including the reviews
-place_details = gmaps.place(place_id, fields=['reviews'])
-place_name = gmaps.place(place_id, fields=['name'])['result']['name']
-# Extract the reviews
-reviews = place_details['result']['reviews']
+app = Flask(__name__)
 
-# Print the reviews
-for review in reviews:
-    counter = counter + 1
-    print('Author:', review['author_name'])
-    print('Rating:', review['rating'])
-    print('Time:', datetime.fromtimestamp(review['time']))
 
-print(place_name + " Number of Reviews: "+str(counter))
-requests.put('https://polar-fortress-89665.herokuapp.com/', place_name)
+@app.route('/count_reviews')
+def count_reviews():
+    counter = 0
+    # Replace key='[insert your key without brackets]'
+    google_maps_client = googlemaps.Client(key='AIzaSyDDKS_pV3ihaftl-2JRVEpqMlOZe8I0yp4')
+    # Specify the place ID of the business
+    place_id = 'ChIJ7fLdskyTToYR81Dj4KgYsds'
+    # Get the place details, including the reviews
+    place_details = google_maps_client.place(place_id, fields=['reviews'])
+    place_name = google_maps_client.place(place_id, fields=['name'])['result']['name']
+    # Extract the reviews
+    reviews = place_details['result']['reviews']
+
+    # Print the reviews
+    for review in reviews:
+        counter = counter + 1
+        print('Author:', review['author_name'])
+        print('Rating:', review['rating'])
+        print('Time:', datetime.fromtimestamp(review['time']))
+
+    print(place_name + " Number of Reviews: "+str(counter))
+
+
+if __name__ == '__main__':
+    app.run()
+
