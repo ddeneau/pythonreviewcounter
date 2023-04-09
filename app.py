7e46702bc150127ea770c19a240e2128815d81f6
Danny <ddeneau@terpmail.umd.edu>
2023-04-09 02:05:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def count_reviews():
-    return "32"
+    return jsonify({"":""})
 
 
 if __name__ == '__main__':
