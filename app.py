9508b0063a8c36ee3323c999b40be0e665bc70e4
Danny <ddeneau@terpmail.umd.edu>
2023-04-17 14:40:33 -0400

    Changed code to use google API

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, jsonify
+import googlemaps
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({
-        'hello': 'world'
-    })
+    # Replace key='[insert your key without brackets]'
+    google_maps_client = googlemaps.Client(key='AIzaSyDDKS_pV3ihaftl-2JRVEpqMlOZe8I0yp4')
+    # Specify the place ID of the business
+    place_id = 'ChIJ-Y7WtKnrToYRnPvOPI0dwJI'
+    # Get the place details, including the number of reviews and name.
+    review_count = \
+        google_maps_client.place(place_id,
+                                 fields=['user_ratings_total'])['result']['user_ratings_total']
+    place_name = google_maps_client.place(place_id, fields=['name'])['result']['name']
+
+    print(place_name + " Number of Reviews: " + str(review_count))
+
+    return jsonify({place_name + "reviews": str(review_count)})
 
 
 if __name__ == '__main__':
