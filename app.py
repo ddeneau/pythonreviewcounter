b35b793eaf6d701ca527ca20b2bf2bf4727c1504
Danny <ddeneau@terpmail.umd.edu>
2023-04-17 15:28:03 -0400

    Might be finally working. . .

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, jsonify
 
-
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods = ['GET'])
 def index():
-    return jsonify({'number' : 43})
+    data = {"number" : 43}
+    return jsonify(data)
 
 
 if __name__ == '__main__':
