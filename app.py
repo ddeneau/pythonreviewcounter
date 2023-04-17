ed5e52d0fef4791599fc7c9be05dd75a466c56f3
Danny <ddeneau@terpmail.umd.edu>
2023-04-17 14:49:16 -0400

    Added error handling for bad API key or any api error.

--- app.py
+++ app.py
@@ -6,22 +6,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Replace key='[insert your key without brackets]'
-    google_maps_client = googlemaps.Client(key='AIzaSyDDKS_pV3ihaftl-2JRVEpqMlOZe8I0yp4')
-    # Specify the place ID of the business
-    place_id = 'ChIJ-Y7WtKnrToYRnPvOPI0dwJI'
-    # Get the place details, including the number of reviews and name.
-    review_count = \
-        google_maps_client.place(place_id,
-                                 fields=['user_ratings_total'])['result']['user_ratings_total']
-    place_name = google_maps_client.place(place_id, fields=['name'])['result']['name']
+    json_to_return = jsonify({'Number' : 32 })
+    try:
+        # Replace key='[insert your key without brackets]'
+        google_maps_client = googlemaps.Client(key='AIzaSyDDKS_pV3ihaftl-2JRVEpqMlOZe8I0yp5')
+        # Specify the place ID of the business
+        place_id = 'ChIJ-Y7WtKnrToYRnPvOPI0dwJI'
+        # Get the place details, including the number of reviews and name.
+        review_count = \
+            google_maps_client.place(place_id,
+                                    fields=['user_ratings_total'])['result']['user_ratings_total']
+        place_name = google_maps_client.place(place_id, fields=['name'])['result']['name']
 
-    print(place_name + " Number of Reviews: " + str(review_count))
+        json_to_return = jsonify({place_name + "reviews": str(review_count)})
+        print(place_name + " Number of Reviews: " + str(review_count))
+    except: googlemaps.exceptions.ApiError
 
-    return jsonify({place_name + "reviews": str(review_count)})
+    return json_to_return
 
 
 if __name__ == '__main__':
-    app.set("")
     app.run()
 
