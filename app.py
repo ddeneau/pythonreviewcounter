ae64ea32c2d6a1a17b0bb6c63681c3ba457273bb
Danny <ddeneau@terpmail.umd.edu>
2023-04-18 13:18:57 -0400

    Added API

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, jsonify
+import googlemaps
 
 app = Flask(__name__)
 
 
 @app.route('/', methods = ['GET'])
 def index():
-    #google_maps_client = googlemaps.Client(key='AIzaSyCJ65EMd3D6oa4udCjuCk2fqGbl0qHe7ec')
-    #place_id = 'ChIJ-Y7WtKnrToYRnPvOPI0dwJI'
-    #review_count = google_maps_client.place(place_id, fields=['user_ratings_total'])['result']['user_ratings_total']
-    review_count = 45
+    google_maps_client = googlemaps.Client(key='AIzaSyCJ65EMd3D6oa4udCjuCk2fqGbl0qHe7ec')
+    place_id = 'ChIJ-Y7WtKnrToYRnPvOPI0dwJI'
+    review_count = google_maps_client.place(place_id, fields=['user_ratings_total'])['result']['user_ratings_total']
     data = {"number" : review_count}
     return jsonify(data)
 
