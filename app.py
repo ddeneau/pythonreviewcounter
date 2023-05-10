4898affca03693ec0601b59b67973200d58c4622
Danny <ddeneau@terpmail.umd.edu>
2023-05-10 16:18:12 -0400

    Updated key

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 def index():
-    google_maps_client = googlemaps.Client(key='AIzaSyCJ65EMd3D6oa4udCjuCk2fqGbl0qHe7ec')
+    google_maps_client = googlemaps.Client(key='YOUR_KEY')
     place_id = 'ChIJ-Y7WtKnrToYRnPvOPI0dwJI'
     review_count = google_maps_client.place(place_id, fields=['user_ratings_total'])['result']['user_ratings_total']
     data = {"number" : review_count}
